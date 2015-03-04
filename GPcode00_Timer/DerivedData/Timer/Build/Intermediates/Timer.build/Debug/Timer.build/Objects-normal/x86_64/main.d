9fab0d32f3b9cfa75e125c3af996d5bc97e20aaf
dknife <ymkang@San302-YMKang.local>
2015-03-04 17:22:37 +0900

    Timer Project

---
+++ GPcode00_Timer/DerivedData/Timer/Build/Intermediates/Timer.build/Debug/Timer.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/Lecture_GamePhysics/GPcode00_Timer/main.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/Lecture_GamePhysics/GPcode00_Timer/StopWatch.h
