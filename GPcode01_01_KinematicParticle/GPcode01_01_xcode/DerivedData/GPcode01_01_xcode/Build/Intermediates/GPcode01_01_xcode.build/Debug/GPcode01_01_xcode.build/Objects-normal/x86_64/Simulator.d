00b01594027b8698c456cd1553bc0985074101c2
dknife <ymkang@San302-YMKang.local>
2015-03-27 22:00:28 +0900

    Xcode Project Created

---
+++ GPcode01_01_KinematicParticle/GPcode01_01_xcode/DerivedData/GPcode01_01_xcode/Build/Intermediates/GPcode01_01_xcode.build/Debug/GPcode01_01_xcode.build/Objects-normal/x86_64/Simulator.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/Simulator.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/Simulator.h
