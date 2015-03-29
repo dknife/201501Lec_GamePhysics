d890a0235c5ab824aeddc0019850272c68235c2a
dknife <ymkang@San302-YMKang.local>
2015-03-29 11:38:53 +0900

    compiled on mH

--- GPcode01_01_KinematicParticle/GPcode01_01_xcode/DerivedData/GPcode01_01_xcode/Build/Intermediates/GPcode01_01_xcode.build/Debug/GPcode01_01_xcode.build/Objects-normal/x86_64/StopWatch.d
+++ GPcode01_01_KinematicParticle/GPcode01_01_xcode/DerivedData/GPcode01_01_xcode/Build/Intermediates/GPcode01_01_xcode.build/Debug/GPcode01_01_xcode.build/Objects-normal/x86_64/StopWatch.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/mi-nyeonghwang/Desktop/ymkang/WorkAndDevelopment/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/StopWatch.cpp \
-  /Users/mi-nyeonghwang/Desktop/ymkang/WorkAndDevelopment/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/StopWatch.h
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/StopWatch.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode01_01_KinematicParticle/GPcode01_01_KinematicParticle/StopWatch.h
