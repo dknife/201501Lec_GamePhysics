ec637fed3d1120cd0f5cda0f0c7fb290d188111c
dknife <ymkang@San302-YMKang.local>
2015-04-02 14:05:06 +0900

    xcode project

---
+++ GPcode01_02_KinematicParticleRun/GPCode0201_xcode/DerivedData/GPCode0201_xcode/Build/Intermediates/GPCode0201_xcode.build/Debug/GPCode0201_xcode.build/Objects-normal/x86_64/Vec3d.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode01_02_KinematicParticleRun/GPcode01_02_KinematicParticleRun/Vec3d.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode01_02_KinematicParticleRun/GPcode01_02_KinematicParticleRun/Vec3d.h
