4009a37e5288ad2f1d63b991ed627b1ea6040c0e
dknife <ymkang@San302-YMKang.local>
2015-04-23 09:56:03 +0900

    2D rigid body

---
+++ GPcode04_01_2DRigid/GPcode0401_Xcode/DerivedData/GPcode0401_Xcode/Build/Intermediates/GPcode0401_Xcode.build/Debug/GPcode0401_Xcode.build/Objects-normal/x86_64/DynamicSimulator.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/DynamicSimulator.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/DynamicSimulator.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/Simulator.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/Hovercraft.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/Vec3d.h
---
+++ GPcode04_01_2DRigid/GPcode0401_Xcode/DerivedData/GPcode0401_Xcode/Build/Intermediates/GPcode0401_Xcode.build/Debug/GPcode0401_Xcode.build/Objects-normal/x86_64/Simulator.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/Simulator.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode04_01_2DRigid/GPcode04_01_2DRigid/Simulator.h
