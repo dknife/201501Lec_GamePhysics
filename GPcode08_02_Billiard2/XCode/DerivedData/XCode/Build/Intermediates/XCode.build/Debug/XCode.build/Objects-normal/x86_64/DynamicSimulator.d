0d5df5696567eac1fb81ac56962f0fee459edb5e
dknife <ymkang@San302-YMKang.local>
2015-06-08 19:05:30 +0900

    t

--- GPcode08_02_Billiard2/XCode/DerivedData/XCode/Build/Intermediates/XCode.build/Debug/XCode.build/Objects-normal/x86_64/DynamicSimulator.d
+++ GPcode08_02_Billiard2/XCode/DerivedData/XCode/Build/Intermediates/XCode.build/Debug/XCode.build/Objects-normal/x86_64/DynamicSimulator.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/DynamicSimulator.cpp \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/DynamicSimulator.h \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.h \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/Particle.h \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/Vec3d.h
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/DynamicSimulator.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/DynamicSimulator.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/Particle.h \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/Vec3d.h
--- GPcode08_02_Billiard2/XCode/DerivedData/XCode/Build/Intermediates/XCode.build/Debug/XCode.build/Objects-normal/x86_64/Simulator.d
+++ GPcode08_02_Billiard2/XCode/DerivedData/XCode/Build/Intermediates/XCode.build/Debug/XCode.build/Objects-normal/x86_64/Simulator.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.cpp \
-  /Users/ymkang/myWork_iMac/Mac_2_Lectures/201501Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.h
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.cpp \
+  /Users/ymkang/myWork_iMac/Mac_2_Lectures/2015Lec_GamePhysics/GPcode08_02_Billiard2/Source/Simulator.h
